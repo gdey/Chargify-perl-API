adfdecb6df6de7fba016b565d58bad7f6ed56ff1
Jimi Damon <jdamon@gmail.com>
2012-06-16 13:28:19 -0700

    ENH: additional date time parsing capabilities

--- lib/WWW/Chargify/Utils/DateTime.pm
+++ lib/WWW/Chargify/Utils/DateTime.pm
@@ -3,10 +3,19 @@ package WWW::Chargify::Utils::DateTime;
 our $VERSION = version->declare('v0.0.1');
 use Moose;
 use Moose::Util::TypeConstraints;
+use Date::Parse;
 use DateTime::Format::W3CDTF;
 
  coerce 'DateTime' => from 'Str' => via { 
-       DateTime::Format::W3CDTF->new->parse_datetime($_);
+     my $retval;
+     eval {
+         $retval = DateTime::Format::W3CDTF->new->parse_datetime($_);
+         return $retval;
+     };
+     
+     # Try again with the other format
+     my $tmp = Date::Parse::str2time( $_ );
+     DateTime->from_epoch( epoch => $tmp );
  };
 
 1;
